7261c11d11fab3f0c64ae4cbc102e0e5af642a2d
j-lagr <joshualagrata@gmail.com>
2023-02-26 21:44:25 +0800

    selection sort function

--- sort.py
+++ sort.py
@@ -2,5 +2,15 @@
 array =[15,55,14,37,66,78,6,89,30,56]
 
 def selectionsort(array):
-    
-        
+    for i in range (10):
+        firstnum = i
+        for n in range (i,11):
+            if array[n] < array[firstnum]:
+                firstnum = n
+        var = array[i]
+        array[i] = array[firstnum]
+        array[firstnum]= var
+        print(array)
+
+selectionsort(array)
+
