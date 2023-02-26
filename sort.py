32718db94ce9bee0db0a1dbd1cadda77d80117b1
j-lagr <joshualagrata@gmail.com>
2023-02-26 21:52:07 +0800

    finished

--- sort.py
+++ sort.py
@@ -1,10 +1,10 @@
 #15, 55, 14, 37, 66, 78, 6, 89, 30, 56
 array =[15,55,14,37,66,78,6,89,30,56]
-
+print("Sorting....")
 def selectionsort(array):
-    for i in range (10):
+    for i in range (9):
         firstnum = i
-        for n in range (i,11):
+        for n in range (i,10):
             if array[n] < array[firstnum]:
                 firstnum = n
         var = array[i]
@@ -13,4 +13,13 @@ def selectionsort(array):
         print(array)
 
 selectionsort(array)
-
+final =str(array)
+final = final.replace("[","")
+final = final.replace("]","")
+print()
+print("--------------------------------------------------")
+print("|                 Selection Sort                 |")
+print("|                                                |")
+print("|    ",final,"     |")
+print("|                                                |")
+print("--------------------------------------------------")
